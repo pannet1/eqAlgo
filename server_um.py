732df28a0490d59b21b98816834358aba9c74cdd
karthick <prog@ecomsense.in>
2022-06-19 20:57:39 +0530

    modify order by ot condition

--- server_um.py
+++ server_um.py
@@ -19,7 +19,7 @@ Routes
 4. /bt/symbol/target - adjust target for bracket order
 5. /be/symbol - exit bracket order
 6. /me/symbol - exit MIS order by symbol
-7. /ne/symbol - exit NRML order by symbol
+7. /ne/symbol - exit MIS order by symbol
 To exit the first order only, add ?mode=first like
 /be/symbol/stop?first=True
 """
@@ -207,7 +207,7 @@ def mis_exit(symbol):
 @app.route('/ne/<symbol>', methods=['GET'])
 def nrml_exit(symbol):
     """
-    Exit NRML order by symbol
+    Exit MIS order by symbol
     """
     responses = []
     symbol = str(symbol).upper()
@@ -258,14 +258,14 @@ def positions():
             print(f"{client_id}|Current PnL:{int(mtm)}|Max PnL:{int(max_mtm)}|DD:{dd :.2f}%")
             if user.must_exit_all:
                 if user.broker.pending_orders():
-                    print('Triggering panic - closing all BO orders')
+                    print('Triggering panic - closing all bracket orders')
                     user.exit_all_bracket_orders()
                     user.broker.cancel_all_orders_by_conditions()
                     #requests.get(f"http://127.0.0.1:8181/disable/{client_id}")
                 else:
                     print('No pending orders to exit')
                 user.exit_all_positions()
-            return pos
+        return pos
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(users)) as executor:
         future_to_result = {executor.submit(check_and_close, user) for user in USERS}
         for future in concurrent.futures.as_completed(future_to_result):
@@ -417,24 +417,20 @@ def modify(varargs):
     """
     responses = []
     filter_args = transform(varargs)
-    ot = filter_args.pop('ot', '')
-    exchange = filter_args.get('exchange', 'NFO')
+    exchange = filter_args.get('exchange', 'NSE')
     # Check for modifications
     price = filter_args.pop('price', 0)
     quantity = filter_args.pop('quantity', 0)
     trigger_price = filter_args.pop('trigger_price', 0)
-    if trigger_price:
-        filter_args['status'] = 'trigger pending'
-    else:
-        filter_args['status'] = 'open'
     n = filter_args.pop('n', 0)
     modifications = {}
-    if len(ot) > 0:
-        modifications['ot'] = ot
+    if price:
+        modifications['price' ] = price
     if quantity:
         modifications['quantity'] = quantity
-    modifications['price' ] = price
-    modifications['trigger_price'] = trigger_price
+    if trigger_price:
+        modifications['trigger_price'] = trigger_price
+
     for user in USERS:
         if user not in DISABLED_USERS:
             response = user.broker.modify_all_orders_by_conditions(modifications,
@@ -450,14 +446,13 @@ def cancel(varargs):
     responses = []
     filter_args = transform(varargs)
     n = filter_args.pop('n', 0)
-    exchange = filter_args.get('exchange', 'NFO')
+    exchange = filter_args.get('exchange', 'NSE')
     # Check for modifications
     for user in USERS:
         if user not in DISABLED_USERS:
             response = user.broker.cancel_all_orders_by_conditions(n=n,**filter_args)
             responses.append(response)
     return str(responses)
-
 bo_string = "exc=NSE/sym=TATAPOWER-EQ/qty=4/val=DAY/sq_val=1/sl_val=1/pr=82.8/tsl=1/ot=LIMIT/prd=BO/side=BUY/user_order_id=10003"
 
 if __name__ == "__main__":
